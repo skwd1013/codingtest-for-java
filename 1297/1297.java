34980cb7d54f201e10749d33aadcaa92c3f58244
skwd1013 <43905686+skwd1013@users.noreply.github.com>
2023-11-21 19:03:49 +0900

    Create 1297.java

---
+++ 1297/1297.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int D=sc.nextInt();
+		int H=sc.nextInt();
+		int W=sc.nextInt();
+		double a=((double)D*(double)D/((double)H*(double)H+(double)W*(double)W));
+	    double b= Math.sqrt(a);
+		double realW=b*W;
+		double realH=b*H;
+
+		System.out.println((int)realH); 
+		System.out.println((int)realW);
+		
+	}
+}
