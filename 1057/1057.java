16cfa50940feb5226b2eb2fd2ef1711d8919135b
skwd1013 <skwd1013@naver.com>
2023-11-24 15:11:31 +0900

    1057

--- 1057/1057.java
+++ 1057/1057.java
@@ -1,4 +1,3 @@
-//수정중
 import java.util.Scanner;
 
 public class Main
@@ -6,25 +5,20 @@ public class Main
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    
+	    int N=sc.nextInt();
 	    int a=sc.nextInt();
 	    int b=sc.nextInt();
-	    int N=sc.nextInt();
-
-	    double aa;
-	    double bb;
-	    double result=0;
 	    
-	        for (int j=0;j<N/2+1;j++){
-	            if (result!=1)   {
-	            aa=Math.ceil(2/a);
-	            bb=Math.ceil(2/b);
-	            result=bb-aa;
-	            	            }
-	           else{
-	            System.out.println(j);
-	                }
+	    int count=0;
+	    
+	    while(a!=b){
+	        a=a/2+a%2;
+	        b=b/2+b%2;
+	        count++;
 	    }
-	   // double j= Math.ceil(a/2.0);
-
+	    System.out.println(count);
 	}
 }
+	        
+  
+  
