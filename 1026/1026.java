1a80477a9d822a723f8adb77b20775bbf7206e39
skwd1013 <skwd1013@naver.com>
2023-12-20 11:06:39 +0900

    1026

---
+++ 1026/1026.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        Integer[] B= new Integer[N];
+        
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            B[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(A);
+        Arrays.sort(B,Collections.reverseOrder()); 
+
+        long S = 0;
+        for (int i = 0; i < N; i++) {
+            S += (long) A[i] * B[i]; 
+        }
+
+        System.out.println(S);
+    }
+}
+}
